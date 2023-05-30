2a4c1200db2ec8f09f57e64fa07c9721e32363ab
janinepa <jp092@hdm.stuttgart.de>
2023-05-30 12:15:59 +0200

    added tests - closes #2

--- project/testing.py
+++ project/testing.py
@@ -14,13 +14,24 @@ def test_sql():
 #    transformed = transfrom(data)  
 #    assert transformed.shaped ==(2, 3)
 
-def test_load():
+def test_weather_load():
     data = pd.DataFrame([],columns=['date', 'hour', 'temp', 'dwpt', 'rhum', 'prcp',
             'snow', 'wdir', 'wspd', 'wpgt', 'pres', 'tsun', 'coco','station'])
     load(data, 'weather','test.sqlite')
     
     conn = sqlite3.connect('test.sqlite')
     result = pd.read_sql_query("SELECT * FROM weather", conn)
+    conn.close()
+    
+    assert_frame_equal(result, data)
+    
+def test_train_load():
+    data = pd.DataFrame([],columns=['name', 'eva', 'ds100', 'db', 'creationts', 'meta',
+            'updatets'])
+    load(data, 'train_stations','test.sqlite')
+    
+    conn = sqlite3.connect('test.sqlite')
+    result = pd.read_sql_query("SELECT * FROM train_stations", conn)
     conn.close()
     
     assert_frame_equal(result, data)
